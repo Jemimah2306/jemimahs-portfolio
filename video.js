c75fd0a3231d07b4bbc617d24d8dbce55addc24c
Jemimah Adamu <jemimah.adamu@ontariotechu.net>
2025-10-03 05:07:10 -0400

    Add comments to video.js

--- video.js
+++ video.js
@@ -1,6 +1,9 @@
+// Get the video element by its ID
 var myVideo = document.getElementById("video");
+// Function to play or pause the video when the button is clicked
 function playPause() 
-{
-    if (myVideo.paused) myVideo.play();
-    else myVideo.pause();
+{ 
+    // Check if the video is currently paused
+    if (myVideo.paused) myVideo.play(); // If paused, play the video
+    else myVideo.pause(); // If playing, pause the video
 }
